3d46e87f8bc2b858e3f0393736e0e85185ae58a1
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-04-25 14:11:56 +0300

    create user model

--- source/database/base/models/authorization.py
+++ source/database/base/models/authorization.py
@@ -44,3 +44,20 @@ class UserGroupModel(Base):
     
     def __repr__(self):
         return f"<UserGroupModel(id={self.id}, name={self.name})>"
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, authoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    _hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False)
+    group: Mapped["UserGroupModel"] = relationship("UserGroupModel", back_populates="users")
