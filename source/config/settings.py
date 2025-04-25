e0014246ef4647204715e7018cf22ab147d6d194
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-04-25 13:08:11 +0300

    create config folder with settings and dependencies files also create database folder, create file for run sqlite session

---
+++ source/config/settings.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+
+class BaseAppSettings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).parent.parent
+    PATH_TO_DB: str = str(BASE_DIR / "database" / "theater.db")
