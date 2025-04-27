3e83f0e90e57e271c8414501a5c6c5cc9d4cfe44
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-04-27 17:29:33 +0300

    create few shemas

--- source/shemas/account.py
+++ source/shemas/account.py
@@ -8,7 +8,7 @@ class BaseEmailPasswordShema(BaseModel):
     password: str
     
     model_config = {
-        "from_atributes": True
+        "from_attributes": True
     }
     
     @field_validator("email")
@@ -20,3 +20,53 @@ class BaseEmailPasswordShema(BaseModel):
     @classmethod
     def validate_password(cls, value):
         return account_validators.validate_password_strength(value)
+
+
+class UserRegistrationRequestShema(BaseEmailPasswordShema):
+    pass
+
+
+class PasswordResetRequesShema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestShema(BaseEmailPasswordShema):
+    token: str
+
+
+class UserLoginRequestShema(BaseEmailPasswordShema):
+    pass
+
+
+class UserLoginResponseShema(BaseModel):
+    acces_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class UserRegistrationResponseShema(BaseModel):
+    id: int
+    email: EmailStr
+    
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class UserActivationRquestShema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class MessageResponseShema(BaseModel):
+    message: str
+
+
+class TokenRefreshRequestShema(BaseModel):
+    regresh_token: str
+
+
+class TokenRefreshResponseShema(BaseModel):
+    acces_token: str
+    token_type: str = "bearer"
+    
