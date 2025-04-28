62e3bb1edb524cc19db75922e5046281bf016901
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-04-28 12:07:18 +0300

    create exception file, start working with accounts routes

---
+++ source/routes/accounts.py
@@ -0,0 +1,9 @@
+from typing import cast
+from datetime import datetime, timezone
+
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy import select, delete
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+
