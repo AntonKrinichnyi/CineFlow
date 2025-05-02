6712fd604c718fac980244c9d803242b47e29c19
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-05-02 21:35:03 +0300

    create celery task for delete expired tokens from db

--- source/config/settings.py
+++ source/config/settings.py
@@ -13,6 +13,9 @@ class Settings(BaseAppSettings):
     SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
+    
+    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
+    CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
 
 
 class TestSettings(BaseAppSettings):
---
+++ source/notifications/tasks.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from celery import Celery
+
+from source.notifications.celery import celery_app
+from source.database.base.models.accounts import ActivationTokenModel
+from source.database.session_sqlite import AsyncSQLiteSessionLocal
+
+app = Celery("tasks", backend="redis://localhost", broker="redis://localhost")
+
+
+@celery_app.task()
+def delete_expired_activation_tokens():
+    db = AsyncSQLiteSessionLocal
+    try:
+        expired_activation_tokens = db.query(ActivationTokenModel).filter(ActivationTokenModel.expires_at < datetime.now(datetime.timezone.utc)).all()
+        for token in expired_activation_tokens:
+            db.delete(token)
+        
+        db.commit()
+    except Exception:
+        db.rollback()
+        print(f"Error during expired token: {Exception}")
+    finally:
+        db.close()
