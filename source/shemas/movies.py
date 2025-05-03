e9fa27225078004830becd2e974aa40531ece3e3
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-05-03 21:33:53 +0300

    add movie create shema

--- source/shemas/movies.py
+++ source/shemas/movies.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field
 
 
 class GenreShema(BaseModel):
@@ -8,7 +8,7 @@ class GenreShema(BaseModel):
     name: str
     
     model_config = {
-        "from_atributes": True,
+        "from_attributes": True,
     }
 
 
@@ -17,7 +17,7 @@ class DirectorShema(BaseModel):
     name: str
     
     model_config = {
-        "from_atributes": True,
+        "from_attributes": True,
     }
 
 
@@ -26,7 +26,7 @@ class StarShema(BaseModel):
     name: str
     
     model_config = {
-        "from_atributes": True,
+        "from_attributes": True,
     }
 
 
@@ -35,7 +35,7 @@ class CertificationShema(BaseModel):
     name: str
     
     model_config = {
-        "from_atributes": True,
+        "from_attributes": True,
     }
 
 
@@ -51,7 +51,7 @@ class MovieBaseShema(BaseModel):
     price: float
     
     model_config = {
-        "from_atributes": True,
+        "from_attributes": True,
     }
     
     @field_validator("year")
@@ -61,3 +61,55 @@ class MovieBaseShema(BaseModel):
         if value > current_year + 1:
             raise ValueError(f"The year in 'year' cannot be greater than {current_year + 1}.")
         return value
+
+
+class CommentShema(BaseModel):
+    id: int
+    user_id: int
+    comment: str
+    answers: list[int] | None = None
+    
+    model_config = {
+        "from_attributes": True,
+    }
+
+
+class MovieDetailShema(BaseModel):
+    id: int
+    genres: list[GenreShema]
+    stars: list[StarShema]
+    directors: list[DirectorShema]
+    certification: CertificationShema
+    comments: list[CommentShema]
+    
+    model_config = {
+        "from_attributes": True,
+    }
+
+
+class MovieCreateShema(BaseModel):
+    uuid: str | None = None
+    name: str
+    year: int
+    time: int
+    imdb: float = Field(..., ge=0, le=10)
+    meta_score: float | None = None
+    gross: float | None = None
+    description: str
+    price: float = Field(..., ge=0)
+    likes: int
+    dislikes: int
+    genres: list[str]
+    stars: list[str]
+    directors: list[str]
+    certification: str
+    comments: list[CommentShema]
+    
+    model_config = {
+        "from_attributes": True,
+    }
+    
+    @field_validator("")
+    @classmethod
+    def validate_list_friends(cls, value: list[str]) -> list[str]:
+        return [item.title() for item in value]
